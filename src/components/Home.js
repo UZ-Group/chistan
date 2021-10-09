bf99a3c058d43ced690e26356506791a0649dcca
Mahdi A <81549809+Mahdi-Alizadeh96@users.noreply.github.com>
2021-10-10 02:22:05 +0330

    Riddle home prev

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header&Footer/Header/Header';
+import RiddlesList from './layout/RiddlesList';
 
 class Home extends React.Component {
     render() { 
         return (
         <div>
             <Header/>
+            <RiddlesList/>
         </div>
         );
     }
---
+++ src/components/layout/RiddlesList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import RiddlePrev from '../RiddleDet/RiddleHomePrev/RiddlePrev.jsx'
+
+
+function RiddlesList() {
+    return ( 
+        <div className={'riddlesList'}>
+            <RiddlePrev/>
+            <RiddlePrev/>
+            <RiddlePrev/>
+            <RiddlePrev/>
+            <RiddlePrev/>
+        </div>
+     );
+}
+
+export default RiddlesList;
